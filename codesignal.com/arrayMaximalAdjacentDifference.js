158ed2fdd0b2bca445effa45d4e5be8d2517f3df
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-07-20 15:24:20 -0400

    arrayMaximalAdjacentDifference solution

--- codesignal.com/arrayMaximalAdjacentDifference.js
+++ codesignal.com/arrayMaximalAdjacentDifference.js
@@ -18,3 +18,12 @@
 // [output] integer
 
 // The maximal absolute difference.
+
+function arrayMaximalAdjacentDifference(inputArray){
+  let maxAbsDif = 0
+  for(let i=1; i<inputArray.length; i++){
+    const dif = Math.abs(inputArray[i] - inputArray[i -1])
+    if(dif > maxAbsDif) maxAbsDif = dif
+  }
+  return maxAbsDif
+} 
