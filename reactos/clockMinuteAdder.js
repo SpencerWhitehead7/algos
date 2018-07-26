d79ae89754c54673f0de1c3dae4caa7d7bee012f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-07-25 23:50:10 -0400

    clockMinuteAdder prompt

---
+++ reactos/clockMinuteAdder.js
@@ -0,0 +1,19 @@
+// Clock Minute Adder
+// Interviewer Prompt
+// Given:
+
+// a time in string format HH:MM
+// a number of minutes
+// Return:
+
+// The time given those minutes added to the base time
+// Assumptions:
+
+// You're working with a standard 12-hour clock
+// Output needs to match input format HH:MM
+// Example Output
+addMinutes(`1:30`, 30)     // 2:00
+addMinutes(`12:30`, 40)    // 1:10
+addMinutes(`11:59`, 1)     // 12:00
+addMinutes(`1:59`, 240)    // 5:59
+addMinutes(`1:23`, 456789) // 6:32
