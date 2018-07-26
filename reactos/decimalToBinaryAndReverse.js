66ba9ad9aa42de2fd698bd08eba799ca3244e9ae
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-07-26 00:04:37 -0400

    decimalToBinaryAndReverse prompt

---
+++ reactos/decimalToBinaryAndReverse.js
@@ -0,0 +1,12 @@
+// Decimal-To-Binary & reverse
+// Prompt
+// Write 2 functions, one that takes the a number in base 10 (decimal) and converts it to the string representation of that number in base 2 (binary), and one that converts back.
+
+// You may not use parseInt, toString, or any similar function which does base conversion for you.
+
+// Examples
+decimalToBinary(4) // should return '100'
+decimalToBinary(67) // should return '1000011'
+
+binaryToDecimal(`100`) // should return 4
+binaryToDecimal(`1000011`) // should return 67
