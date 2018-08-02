d5bcda98fa54b274585a1adc17ffd73c83be91e5
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-02 15:35:13 -0400

    fix: defines variable

--- codesignal.com/arcade/intro/makeArrayConsecutive2.js
+++ codesignal.com/arcade/intro/makeArrayConsecutive2.js
@@ -25,7 +25,7 @@
 
 function makeArrayConsecutive2(statues){
   statues.sort((a, b)=> a - b)
-  additional = 0
+  let additional = 0
   for(let i=0; i<statues.length - 1; i++){
     additional += statues[i+1] - statues[i] - 1
   }
