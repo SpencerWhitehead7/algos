b18f0c29a613d98e58ae860f6e8985faf692e6c9
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-06 10:34:33 -0400

    7. Reverse Integer solution

--- leetcode.com/easy/7. Reverse Integer.js
+++ leetcode.com/easy/7. Reverse Integer.js
@@ -14,3 +14,14 @@
 // Output: 21
 // Note:
 // Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = function(x){
+  if(x > Math.pow(2, 31) - 1 || x < Math.pow(-2, 31)) return 0
+  const numAsRevStr = x.toString().split(``).reverse().join(``)
+  const revInt = numAsRevStr[numAsRevStr.length - 1] === `-` ? Number(numAsRevStr.slice(0, -1))*-1 : Number(numAsRevStr)
+  return (revInt > Math.pow(2, 31) - 1 || revInt < Math.pow(-2, 31)) ? 0 : revInt
+}
