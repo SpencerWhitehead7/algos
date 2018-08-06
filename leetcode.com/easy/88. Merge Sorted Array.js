35cee71dc115f2ca30214d863382f216fff986a7
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-06 10:20:34 -0400

    88. Merge Sorted Array solution

--- leetcode.com/easy/88. Merge Sorted Array.js
+++ leetcode.com/easy/88. Merge Sorted Array.js
@@ -11,3 +11,24 @@
 // nums2 = [2,5,6],       n = 3
 
 // Output: [1,2,2,3,5,6]
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n){
+  let counter = 0
+  while(counter < n){
+    nums1.pop()
+    counter++
+  }
+  for(let i=0; i<nums1.length; i++){
+    if(nums2[0] < nums1[i]){
+      nums1.splice(i, 0, nums2.shift())
+    }
+  }
+  nums1.push(...nums2)
+}
