8716e83c0e1a89a908ca559e47be22c273c83d18
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-06 10:08:45 -0400

    2. Add Two Numbers solution

--- leetcode.com/easy/2. Add Two Numbers.js
+++ leetcode.com/easy/2. Add Two Numbers.js
@@ -7,3 +7,31 @@
 // Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 // Output: 7 -> 0 -> 8
 // Explanation: 342 + 465 = 807.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2){
+  const start = new ListNode(0)
+  let currentNode = start
+  let carry = 0
+  while(l1 || l2 || carry){
+    const dSum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry
+    const nextNode = new ListNode(dSum%10)
+    carry = Math.floor(dSum/10) !== 0 ? 1 : 0
+    currentNode.next = nextNode
+    currentNode = nextNode
+    if(l1) l1 = l1.next
+    if(l2) l2 = l2.next
+  }
+  return start.next
+}
