52b029dde8e0c8f7755fcdc58fc0436e31d2e6aa
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-06 10:26:38 -0400

    622. Design Circular Queue solution

--- leetcode.com/medium/622. Design Circular Queue.js
+++ leetcode.com/medium/622. Design Circular Queue.js
@@ -31,3 +31,86 @@
 // All values will be in the range of [0, 1000].
 // The number of operations will be in the range of [1, 1000].
 // Please do not use the built-in Queue library.
+
+/**
+ * Initialize your data structure here. Set the size of the queue to be k.
+ * @param {number} k
+ */
+const MyCircularQueue = function(k){
+  this.queue = new Array(k).fill(null)
+  this.head = 0
+  this.tail = 0
+}
+
+/**
+* Insert an element into the circular queue. Return true if the operation is successful. 
+* @param {number} value
+* @return {boolean}
+*/
+MyCircularQueue.prototype.enQueue = function(value){
+  if(this.queue[this.tail] === null){
+    this.queue[this.tail] = value
+    this.tail = this.tail === this.queue.length - 1 ? 0 : this.tail + 1
+    return true
+  }else{
+    return false
+  }
+}
+
+/**
+* Delete an element from the circular queue. Return true if the operation is successful.
+* @return {boolean}
+*/
+MyCircularQueue.prototype.deQueue = function(){
+  if(this.queue[this.head] !== null){
+    this.queue[this.head] = null
+    this.head = this.head === this.queue.length - 1 ? 0 : this.head + 1
+    return true
+  }else{
+    return false
+  }
+}
+
+/**
+* Get the front item from the queue.
+* @return {number}
+*/
+MyCircularQueue.prototype.Front = function(){
+  return this.queue[this.head] !== null ? this.queue[this.head] : -1
+}
+
+/**
+* Get the last item from the queue.
+* @return {number}
+*/
+MyCircularQueue.prototype.Rear = function(){
+  const rear = this.tail === 0 ? this.queue[this.queue.length - 1] : this.queue[this.tail - 1]
+  return rear !== null ? rear : -1
+}
+
+/**
+* Checks whether the circular queue is empty or not.
+* @return {boolean}
+*/
+MyCircularQueue.prototype.isEmpty = function(){
+  return this.queue[this.head] === null
+}
+
+/**
+* Checks whether the circular queue is full or not.
+* @return {boolean}
+*/
+MyCircularQueue.prototype.isFull = function(){
+  return this.queue[this.tail] !== null
+}
+
+/** 
+* Your MyCircularQueue object will be instantiated and called as such:
+* var obj = Object.create(MyCircularQueue).createNew(k)
+* var param_1 = obj.enQueue(value)
+* var param_2 = obj.deQueue()
+* var param_3 = obj.Front()
+* var param_4 = obj.Rear()
+* var param_5 = obj.isEmpty()
+* var param_6 = obj.isFull()
+*/
