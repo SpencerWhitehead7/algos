5241ff2171e228297eda8ea18228ddcc9b590bc0
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-06 10:29:34 -0400

    28. Implement strStr solution

--- leetcode.com/easy/28. Implement strStr.js
+++ leetcode.com/easy/28. Implement strStr.js
@@ -15,3 +15,18 @@
 // What should we return when needle is an empty string? This is a great question to ask during an interview.
 
 // For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = function(haystack, needle){
+  if(needle===``) return 0
+  for(let i=0; i<=haystack.length - needle.length; i++){
+    if(haystack.slice(i, i+needle.length)===needle){
+      return i
+    }
+  }
+  return -1
+}
