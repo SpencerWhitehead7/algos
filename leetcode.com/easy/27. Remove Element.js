7ce5cc7f240ef21432b9e3398df38683f0a6ca6d
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-06 10:31:36 -0400

    27. Remove Element solution

--- leetcode.com/easy/27. Remove Element.js
+++ leetcode.com/easy/27. Remove Element.js
@@ -36,3 +36,20 @@
 // for (int i = 0; i < len; i++) {
 //     print(nums[i]);
 // }
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function(nums, val){
+  let i = 0
+  while(i < nums.length){
+    if(nums[i] === val){
+      nums.splice(i, 1)
+    }else{
+      i++
+    }
+  }
+  return nums.length
+}
