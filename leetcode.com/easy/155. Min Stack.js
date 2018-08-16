80823b44b47653a67c82757361f504d1848a76e4
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-16 10:42:11 -0400

    155. Min Stack solution

--- leetcode.com/easy/155. Min Stack.js
+++ leetcode.com/easy/155. Min Stack.js
@@ -13,3 +13,52 @@
 // minStack.pop();
 // minStack.top();      --> Returns 0.
 // minStack.getMin();   --> Returns -2.
+
+/**
+ * initialize your data structure here.
+ */
+const MinStack = function(){
+  this.stack = []
+}
+
+/** 
+* @param {number} x
+* @return {void}
+*/
+MinStack.prototype.push = function(x){
+  this.stack.push(x)
+}
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function(){
+  this.stack.pop()
+}
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function(){
+  return this.stack[this.stack.length - 1]
+}
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function(){
+  let min = Infinity
+  this.stack.forEach(ele => {
+    if(ele < min) min = ele
+  })
+  return min
+}
+
+/** 
+* Your MinStack object will be instantiated and called as such:
+* var obj = Object.create(MinStack).createNew()
+* obj.push(x)
+* obj.pop()
+* var param_3 = obj.top()
+* var param_4 = obj.getMin()
+*/
