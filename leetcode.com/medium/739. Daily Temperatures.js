ed56aeb6693f1f4f9d81e1994a7eaf02b0e3e949
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-16 10:38:26 -0400

    739. Daily Temperatures solution

--- leetcode.com/medium/739. Daily Temperatures.js
+++ leetcode.com/medium/739. Daily Temperatures.js
@@ -3,3 +3,20 @@
 // For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
 
 // Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+const dailyTemperatures = function(temperatures){
+  const res = []
+  const stack = []
+  for(let i=temperatures.length-1; i>=0; i--){
+    while(stack.length>0 && temperatures[stack[stack.length-1]]<=temperatures[i]){
+      stack.pop()
+    }
+    res.unshift(stack[stack.length-1] ? stack[stack.length-1] - i : 0)
+    stack.push(i)
+  }
+  return res
+}
