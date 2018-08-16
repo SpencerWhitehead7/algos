481856772c28330dd0ed7bbaab4534538a6cb261
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-16 14:53:48 -0400

    Magic Number solution

--- Me/magicNumber.js
+++ Me/magicNumber.js
@@ -8,3 +8,15 @@
 // f(1, []) = false
 // f(1, [1]) = true
 // f(0, [1]) = false
+
+const magicNum = (num, arr) => {
+  let allNums = [0]
+  for(let i=0; i<arr.length; i++){
+    const temp = [...allNums]
+    allNums = []
+    for(let j=0; j<temp.length; j++){
+      allNums.push(temp[j] + arr[i], temp[j] - arr[i])
+    }
+  }
+  return allNums.includes(num)
+}
