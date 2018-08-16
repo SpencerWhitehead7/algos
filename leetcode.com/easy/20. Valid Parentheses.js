f04c6ee90ccd7abe01dcfc5ce4fbdbfaaefe9c2b
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-16 10:43:33 -0400

    20. Valid Parentheses solution

--- leetcode.com/easy/20. Valid Parentheses.js
+++ leetcode.com/easy/20. Valid Parentheses.js
@@ -26,3 +26,25 @@
 
 // Input: "{[]}"
 // Output: true
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function(s){
+  const stack = []
+  for(let i=0; i<s.length; i++){
+    if(s[i]===`(` ||
+       s[i]===`{` ||
+       s[i]===`[`){
+      stack.push(s[i])
+    }else if(s[i] === `)` && stack[stack.length - 1] === `(` ||
+             s[i] === `}` && stack[stack.length - 1] === `{` ||
+             s[i] === `]` && stack[stack.length - 1] === `[`){
+      stack.pop()
+    }else{
+      return false
+    }
+  }
+  return stack.length === 0
+}
