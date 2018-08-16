2bdc309aba9c2d7a791ab96ebc5ff3632f8a874d
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-16 10:37:25 -0400

    279. Perfect Squares solution

--- leetcode.com/medium/279. Perfect Squares.js
+++ leetcode.com/medium/279. Perfect Squares.js
@@ -10,3 +10,28 @@
 // Input: n = 13
 // Output: 2
 // Explanation: 13 = 4 + 9.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const numSquares = function(n){
+  const tried = new Set()
+  const steps = [0]
+  const remainders = [n]
+  while(remainders.length > 0){
+    const base = remainders.shift()
+    const step = steps.shift() + 1
+    let minus = 1
+    while(base - Math.pow(minus, 2) >= 0){
+      const remainder = base - Math.pow(minus, 2)
+      if(remainder === 0) return step
+      if(!tried.has(remainder)){
+        tried.add(remainder)
+        remainders.push(remainder)
+        steps.push(step)
+      }
+      minus++
+    }
+  }
+}
