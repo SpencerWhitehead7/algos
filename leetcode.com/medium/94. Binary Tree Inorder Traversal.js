645cfc01dd06f843348ec4025935832928d549f4
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-20 22:22:26 -0400

    94. Binary Tree Inorder Traversal solution (rec)

--- leetcode.com/medium/94. Binary Tree Inorder Traversal.js
+++ leetcode.com/medium/94. Binary Tree Inorder Traversal.js
@@ -11,3 +11,23 @@
 
 // Output: [1,3,2]
 // Follow up: Recursive solution is trivial, could you do it iteratively?
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const inorderTraversal = function(root){
+  const res = []
+  if(!root) return res
+  if(root.left) res.push(...inorderTraversal(root.left))
+  res.push(root.val)
+  if(root.right) res.push(...inorderTraversal(root.right))
+  return res
+}
