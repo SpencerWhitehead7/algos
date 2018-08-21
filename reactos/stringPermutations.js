055e76cdd877ed4636b2ff967e4805373ed92561
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-21 09:19:39 -0400

    stringPermutations solution

--- reactos/stringPermutations.js
+++ reactos/stringPermutations.js
@@ -75,3 +75,31 @@
 
 // Without sorting before we start finding permutations, we will get n! * log(n!) -- we have an array that is n! in length at that point. If we sort before our sort time is n * log(n). In both situations, n is the length of the input string. Overall, finding all string permutations is n!
 
+// finds all possible permutations *while* maintaining the order of the characters
+function stringPermutations (str){
+  if(str.length === 1) return [str] // base case
+  const all = []
+  // go through each character in the string
+  let i = 0
+  while(i < str.length){
+    // get each individual character
+    const letter = str[i]
+    // get all the other characters surrounding it
+    const otherChars = str.slice(0, i) + str.slice(i + 1)
+    // compute all permutations of the *other* characters
+    stringPermutations(otherChars).forEach(submpermut => {
+      // add the current letter to the front of each of these "sub-permutations"
+      // include *that* into the full result set
+      all.push(letter + submpermut)
+    })
+    // increment until we reach a new letter (to avoid duplicates in the result set)
+    while(str[i] === letter) i++
+  }
+  return all
+}
+function sortedStringPermutations (str){
+  // first sort the characters in the string
+  const sortedStr = str.split(``).sort().join(``)
+  // then find the ordered permutations of that sorted string
+  return stringPermutations(sortedStr)
+}
