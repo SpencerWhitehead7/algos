66bc51b3de13e9bc76e954f979fed0f4fd3a4065
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-23 14:22:07 -0400

    268. Missing Number solution

--- leetcode.com/easy/268. Missing Number.js
+++ leetcode.com/easy/268. Missing Number.js
@@ -10,3 +10,23 @@
 // Output: 8
 // Note:
 // Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const missingNumber = function(nums){
+  const hash = {}
+  nums.forEach(num => {
+    hash[num] = 1
+  })
+  for(let i=0; i<=nums.length; i++){
+    if(!hash[i]) return i
+  }
+}
+
+const missingNumberPureMath = function(nums){
+  const expectedSum = (nums.length * (nums.length + 1)) / 2
+  const actualSum = nums.reduce((acc, curr) => acc + curr)
+  return expectedSum - actualSum
+}
