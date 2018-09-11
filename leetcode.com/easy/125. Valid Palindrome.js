230fca006a5fd79e6d413fb5f464ab95d2dadac4
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-11 15:08:25 -0400

    125. Valid Palindrome solution

--- leetcode.com/easy/125. Valid Palindrome.js
+++ leetcode.com/easy/125. Valid Palindrome.js
@@ -10,3 +10,15 @@
 
 // Input: "race a car"
 // Output: false
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = function(s){
+  s = s.replace(/[^a-z0-9]/gmi, ``).toLowerCase()
+  for(let i = 0, j = s.length - 1; i < j; i++, j--){
+    if(s[i] !== s[j]) return false
+  }
+  return true
+}
