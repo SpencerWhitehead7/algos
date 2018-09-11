d0041638fcf778297c3b5fa998d2c0204247df93
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-11 14:13:15 -0400

    234. Palindrome Linked List solution

--- leetcode.com/easy/234. Palindrome Linked List.js
+++ leetcode.com/easy/234. Palindrome Linked List.js
@@ -10,3 +10,42 @@
 // Output: true
 // Follow up:
 // Could you do it in O(n) time and O(1) space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+const reverseLL = head => {
+  if(!head || !head.next) return head
+  const rest = reverseLL(head.next)
+  head.next.next = head
+  head.next = null
+  return rest
+}
+
+const isPalindrome = function(head){
+  if(!head || !head.next) return true
+  let start = head
+  let middle = head
+  let leader = head
+  while(leader && leader.next){
+    middle = middle.next
+    leader = leader.next.next
+  }
+  if(leader) middle = middle.next
+  middle = reverseLL(middle)
+  while(middle){
+    if(start.val !== middle.val) return false
+    start = start.next
+    middle = middle.next
+  }
+  return true
+}
