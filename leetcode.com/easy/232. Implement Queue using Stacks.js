e70d9873304bd9a0b8b667511792822abba2288f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-11 22:34:48 -0400

    232. Implement Queue using Stacks solution

--- leetcode.com/easy/232. Implement Queue using Stacks.js
+++ leetcode.com/easy/232. Implement Queue using Stacks.js
@@ -18,3 +18,69 @@
 // You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
 // Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
 // You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+
+/**
+ * Initialize your data structure here.
+ */
+const MyQueue = function(){
+  this.stack = []
+}
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x){
+  this.stack.push(x)
+}
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function(){
+  const tempStack = []
+  while(this.stack.length > 0){
+    tempStack.push(this.stack.pop())
+  }
+  const res = tempStack.pop()
+  while(tempStack.length > 0){
+    this.stack.push(tempStack.pop())
+  }
+  return res
+}
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function(){
+  const tempStack = []
+  while(this.stack.length > 0){
+    tempStack.push(this.stack.pop())
+  }
+  const res = tempStack.pop()
+  this.stack.push(res)
+  while(tempStack.length > 0){
+    this.stack.push(tempStack.pop())
+  }
+  return res
+}
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function(){
+  return this.stack.length === 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = Object.create(MyQueue).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
