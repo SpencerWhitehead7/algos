e43bb11c8c412f1153ff73424f02afc822bf9a89
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-13 12:41:58 -0400

    rainWaterCollector solution

--- reactos/rainWaterCollector.js
+++ reactos/rainWaterCollector.js
@@ -12,6 +12,27 @@
 
 // Here are some samples to test to be sure the function works. It is helpful to draw them out then walk step-by-step through the solution to help visualize it.
 
+const rainCollector = arr => {
+  const rightMaxes = []
+  let rightMax = 0
+  for(let i = arr.length - 1; i >= 0; i--){
+    rightMax = Math.max(rightMax, arr[i])
+    rightMaxes[i] = rightMax
+  }
+
+  const leftMaxes = []
+  let leftMax = 0
+  for(let i = 0; i < arr.length; i++){
+    leftMax = Math.max(leftMax, arr[i])
+    leftMaxes[i] = leftMax
+  }
+
+  return arr.reduce((waterCollected, block, i) => {
+    const leftMax = leftMaxes[i]
+    const rightMax = rightMaxes[i]
+    return waterCollected + Math.min(leftMax, rightMax) - block
+  }, 0)
+}
 
 
 // // vol = 7
