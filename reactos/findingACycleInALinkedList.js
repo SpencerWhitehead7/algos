fedf23d286f30a28b5a103f9690e1e457eff4962
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-13 12:46:11 -0400

    findingACycleInALinkedList prompt

---
+++ reactos/findingACycleInALinkedList.js
@@ -0,0 +1,12 @@
+// Finding a cycle in a Linked List
+// Slides
+
+// Prompt
+// Given the head of a singly linked list, determine if there is a cycle within the linked list. If there is a cycle, return true and log the length of the cycle and the first node in the cycle. If there is no cycle, return false.
+
+// Caveat: you may not use any additional data structures, nor may you modify the existing data structure. No arrays, no objects, no maps, no linked lists, etc.
+
+// Remember: The nodes in the linked list can have the same values.
+
+// Setup
+// See slides for diagrams and hints!
