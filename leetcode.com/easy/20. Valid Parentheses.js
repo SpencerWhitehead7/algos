6b7a308255fdebf872e42fb544beb9ce2adfa3de
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-13 11:58:25 -0400

    edit: formatting

--- leetcode.com/easy/20. Valid Parentheses.js
+++ leetcode.com/easy/20. Valid Parentheses.js
@@ -33,14 +33,14 @@
  */
 const isValid = function(s){
   const stack = []
-  for(let i=0; i<s.length; i++){
-    if(s[i]===`(` ||
-       s[i]===`{` ||
-       s[i]===`[`){
+  for(let i = 0; i < s.length; i++){
+    if(s[i] === `(` ||
+      s[i] === `{` ||
+      s[i] === `[`){
       stack.push(s[i])
     }else if(s[i] === `)` && stack[stack.length - 1] === `(` ||
-             s[i] === `}` && stack[stack.length - 1] === `{` ||
-             s[i] === `]` && stack[stack.length - 1] === `[`){
+            s[i] === `}` && stack[stack.length - 1] === `{` ||
+            s[i] === `]` && stack[stack.length - 1] === `[`){
       stack.pop()
     }else{
       return false
