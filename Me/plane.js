788584f62b6e99dbff4218db70066dba11356f78
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-21 16:03:16 -0400

    plane prompt

---
+++ Me/plane.js
@@ -0,0 +1,10 @@
+// Write a function that takes a number and a string.
+// The number represents the number of rows on a plane. Each row is arranged into three "blocks" like this:
+
+// ABC DEFG HJK ("I" ommitted for clarity)
+
+// the string represents reserved seats, separated by spaces, in this format RowSeatletter (i.e., 1A or 3F or 10K)
+
+// VERSION 1: the function should return the number of FULL 3-seat "blocks" on the plane. If DEFG are all occupied, only count it as ONE full block.
+
+// VERSION 2: the function should return the number of EMPTY 3-seat "blocks" on the plane. If DEFG are all empty, only count it as ONE empty block.
