8386a42d661b7b433d0d659b414f031bbbab8c40
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-24 21:07:25 -0400

    46. Permutations solution

--- leetcode.com/medium/46. Permutations.js
+++ leetcode.com/medium/46. Permutations.js
@@ -12,3 +12,23 @@
 //   [3,1,2],
 //   [3,2,1]
 // ]
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = nums => {
+  if(nums.length === 1) return [nums]
+  const all = []
+  let i = 0
+  while(i < nums.length){
+    const num = nums[i]
+    const otherNums = [...nums.slice(0, i), ...nums.slice(i + 1)]
+    permute(otherNums).forEach(submpermut => {
+      submpermut.unshift(num)
+      all.push(submpermut)
+    })
+    i++
+  }
+  return all
+}
