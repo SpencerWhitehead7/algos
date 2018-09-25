d5afdcbfb3ae186d0372d39ecd548d4b61830a49
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-24 20:33:45 -0400

    35. Search Insert Position solution

--- leetcode.com/easy/35. Search Insert Position.js
+++ leetcode.com/easy/35. Search Insert Position.js
@@ -18,3 +18,26 @@
 
 // Input: [1,3,5,6], 0
 // Output: 0
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function(nums, target){
+  let start = 0
+  let end = nums.length - 1
+  while(start <= end){
+    const middle = Math.floor((start + end) / 2)
+    if(nums[middle] > target){
+      end = middle - 1
+    }else if(nums[middle] < target){
+      start = middle + 1
+    }else{
+      return middle
+    }
+  }
+  return start
+}
+
+// It's basically binary search
