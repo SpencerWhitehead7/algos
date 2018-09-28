3a30ab63437c54a2ed9aa64663039c00ee6c542c
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-28 14:44:22 -0400

    54. Spiral Matrix solution

--- leetcode.com/medium/54. Spiral Matrix.js
+++ leetcode.com/medium/54. Spiral Matrix.js
@@ -18,3 +18,27 @@
 //   [9,10,11,12]
 // ]
 // Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+const spiralOrder = matrix => {
+  const sorted = []
+  while(matrix.length){
+    sorted.push(...matrix.shift())
+    for(let i = 0; i < matrix.length; i++){
+      const next = matrix[i].pop()
+      if(next !== undefined) sorted.push(next)
+    }
+    sorted.push(...(matrix.pop() || []).reverse())
+    for(let i = matrix.length - 1; i >= 0; i--){
+      const next = matrix[i].shift()
+      if(next !== undefined) sorted.push(next)
+    }
+  }
+  return sorted
+}
+
+// A slightly more rigorous version of snail sort that has to deal with cases like
+// [ [1], [2], [3]] and [ [1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
