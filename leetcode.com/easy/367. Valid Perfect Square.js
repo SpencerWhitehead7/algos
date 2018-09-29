13891496e0c3f69f557c34d4b690bc3cdef81d10
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-29 12:52:10 -0400

    367. Valid Perfect Square

--- leetcode.com/easy/367. Valid Perfect Square.js
+++ leetcode.com/easy/367. Valid Perfect Square.js
@@ -10,3 +10,29 @@
 
 // Input: 14
 // Output: false
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+const isPerfectSquareTrivial = x => Math.sqrt(x) === Math.floor(Math.sqrt(x))
+
+const isPerfectSquare = x => {
+  if(x === 0 || x === 1){
+    return true
+  }
+  let start = 1
+  let end = x
+  while(start < end){
+    const middle = Math.floor((start + end) / 2)
+    const middleSqrd = middle * middle
+    if(middleSqrd === x){
+      return true
+    }else if(middleSqrd > x){
+      end = middle
+    }else if(middleSqrd < x){
+      start = middle + 1
+    }
+  }
+  return false
+}
