da6725afd89e627d9c4f714c0c8d69cc0be09e90
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-29 11:07:20 -0400

    69. Sqrt(x) solution

--- leetcode.com/easy/69. Sqrt(x).js
+++ leetcode.com/easy/69. Sqrt(x).js
@@ -14,3 +14,31 @@
 // Output: 2
 // Explanation: The square root of 8 is 2.82842..., and since
 //              the decimal part is truncated, 2 is returned.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const mySqrtTrivial = x => Math.floor(Math.sqrt(x))
+
+const mySqrt = x => {
+  if(x === 1 || x === 2) return 1
+  let start = 0
+  let end = x
+  while(start < end){
+    const middle = Math.floor((start + end) / 2)
+    const prev = middle - 1
+    const middleSqrd = middle * middle
+    const prevSqrd = prev * prev
+    if(middleSqrd === x){
+      return middle
+    }else if(prevSqrd <= x && middleSqrd > x){
+      return prev
+    }else if(middleSqrd > x){
+      end = middle
+    }else if(middleSqrd < x){
+      start = middle
+    }
+  }
+  return start
+}
