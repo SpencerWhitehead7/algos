edcfbf4f6f9439baf8f7f82da9b5b59cfbb01159
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-30 00:23:18 -0400

    58. Length of Last Word solution

---
+++ leetcode.com/easy/58. Length of Last Word.js
@@ -0,0 +1,27 @@
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+// If the last word does not exist, return 0.
+
+// Note: A word is defined as a character sequence consists of non-space characters only.
+
+// Example:
+
+// Input: "Hello World"
+// Output: 5
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = s => {
+  let length = 0
+  let i = s.length - 1
+  while(s[i] === ` `){
+    i--
+  }
+  while(s[i] !== ` ` && s[i] !== undefined){
+    length++
+    i--
+  }
+  return length
+}
