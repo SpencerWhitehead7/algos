b7f5ef4c75a68f77ad2f116370b117b5deb46ebb
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-05 23:48:48 -0400

    139. Word Break solution

--- leetcode.com/medium/139. Word Break.js
+++ leetcode.com/medium/139. Word Break.js
@@ -19,3 +19,25 @@
 
 // Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 // Output: false
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+const wordBreak = function(s, wordDict){
+  const checked = new Set()
+  const queue = [``]
+  while(queue.length > 0){
+    const base = queue.shift()
+    for(const possibleNextWord of wordDict){
+      const possibleStr = base + possibleNextWord
+      if(possibleStr === s) return true
+      if(s.indexOf(possibleStr) === 0 && !checked.has(possibleStr)){
+        checked.add(possibleStr)
+        queue.push(possibleStr)
+      }
+    }
+  }
+  return false
+}
