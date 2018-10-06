84b2634d12672a596718009034f8cc2d874eeae9
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-06 00:27:28 -0400

    stringsRearrangement solution

--- codesignal.com/arcade/intro/stringsRearrangement.js
+++ codesignal.com/arcade/intro/stringsRearrangement.js
@@ -24,3 +24,46 @@
 // 1 ≤ inputArray[i].length ≤ 15.
 
 // [output] boolean
+
+const stringsRearrangement = inputArray => {
+  const counter = generateCounter(inputArray)
+  const queue = inputArray.map(ele => [ele])
+  while(queue.length){
+    const base = queue.shift()
+    if(base.length === inputArray.length) return true
+    inputArray.forEach(possibleNextStr => {
+      if(countOccurances(possibleNextStr, base) < counter[possibleNextStr] && differsByOne(possibleNextStr, base[base.length - 1])){
+        queue.push([...base, possibleNextStr])
+      }
+    })
+  }
+  return false
+}
+
+const generateCounter = arr => {
+  const counter = {}
+  arr.forEach(ele => {
+    if(counter[ele]){
+      counter[ele]++
+    }else{
+      counter[ele] = 1
+    }
+  })
+  return counter
+}
+
+const countOccurances = (str, arr) => {
+  let occurances = 0
+  arr.forEach(ele => {
+    if(ele === str) occurances++
+  })
+  return occurances
+}
+
+const differsByOne = (s1, s2) => {
+  let differsBy = 0
+  for(let i = 0; i < s1.length; i++){
+    if(s1[i] !== s2[i]) differsBy++
+  }
+  return differsBy === 1
+}
