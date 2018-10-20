d5191b5655f5a8d6057678c9411d00ff91619bb2
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-19 21:38:56 -0400

    Pascal's Triangle #2 solution

--- codewars.com/6/Pascal's Triangle #2.js
+++ codewars.com/6/Pascal's Triangle #2.js
@@ -16,3 +16,20 @@
 //        [1  2  1]
 //       [1  3  3  1]
 // here you get the 3 by adding the 2 and 1 above it.
+
+const pascal = depth => {
+  const res = [[1]]
+  for(let i = 2; i <= depth; i++){
+    const prevLayer = res[res.length - 1]
+    prevLayer.unshift(0)
+    prevLayer.push(0)
+    const nextLayer = []
+    for(let n = 0; n < i; n++){
+      nextLayer.push(prevLayer[n] + prevLayer[n + 1])
+    }
+    prevLayer.shift()
+    prevLayer.pop()
+    res.push(nextLayer)
+  }
+  return JSON.stringify(res)
+}
