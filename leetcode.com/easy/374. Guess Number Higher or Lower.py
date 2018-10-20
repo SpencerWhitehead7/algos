996a48bb459ed8c56b70d7914ba3797abbd53678
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-19 22:41:59 -0400

    374. Guess Number Higher or Lower solution

--- leetcode.com/easy/374. Guess Number Higher or Lower.py
+++ leetcode.com/easy/374. Guess Number Higher or Lower.py
@@ -15,3 +15,28 @@
 
 # Input: n = 10, pick = 6
 # Output: 6
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 1
+        end = n
+        while(True):
+          # leetcode preimported/defined math and guess
+          middle = math.floor((start + end)/2)
+          api_res = guess(middle)
+          if(api_res == 0):
+            return int(middle)
+          if(api_res == 1):
+            start = middle + 1
+          if(api_res == -1):
+            end = middle - 1
+            
