8b74177f63f7b56df62368e82c94f1beebebb992
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-22 23:50:28 -0400

    112. Path Sum solution

--- leetcode.com/easy/112. Path Sum.js
+++ leetcode.com/easy/112. Path Sum.js
@@ -14,3 +14,25 @@
 //  /  \      \
 // 7    2      1
 // return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+const hasPathSum = (root, sum, memo = 0) => {
+  if(!root) return false
+  memo += root.val
+  if(!root.left && !root.right){
+    return sum === memo
+  }else{
+    return hasPathSum(root.left, sum, memo) || hasPathSum(root.right, sum, memo)
+  }
+}
