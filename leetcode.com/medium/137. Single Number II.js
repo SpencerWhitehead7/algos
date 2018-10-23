9d63ec36703182ac36f4c02067cebb34537bb0cc
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-23 15:29:41 -0400

    137. Single Number II solution

--- leetcode.com/medium/137. Single Number II.js
+++ leetcode.com/medium/137. Single Number II.js
@@ -2,7 +2,7 @@
 
 // Note:
 
-// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? // Nope, but I assume it also uses XOR
 
 // Example 1:
 
@@ -12,3 +12,17 @@
 
 // Input: [0,1,0,1,0,1,99]
 // Output: 99
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = nums => {
+  const uniques = new Set(nums)
+  const totalSum = nums.reduce((acc, curr) => acc + curr)
+  let setSum = 0
+  for(const num of uniques){
+    setSum += num
+  }
+  return ((setSum * 3) - totalSum) / 2
+}
