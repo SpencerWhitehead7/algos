2169d1af50a97e997e0b6626952aed638fbccdd3
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-23 00:40:33 -0400

    113. Path Sum II solution rec

--- leetcode.com/medium/113. Path Sum II.js
+++ leetcode.com/medium/113. Path Sum II.js
@@ -19,3 +19,28 @@
 //    [5,4,11,2],
 //    [5,8,4,5]
 // ]
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+const pathSumRec = (root, sum, memo = [], res = []) => {
+  if(!root) return []
+  const newMemo = memo.map(ele => ele)
+  newMemo.push(root.val)
+  if(!root.left && !root.right && newMemo.reduce((acc, curr) => acc + curr) === sum){
+    res.push(newMemo)
+  }else{
+    pathSumRec(root.left, sum, newMemo, res)
+    pathSumRec(root.right, sum, newMemo, res)
+  }
+  return res
+}
