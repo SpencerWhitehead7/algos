2aa5d54ce1dd7544927e266ce7694057e1c4101d
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-23 14:03:06 -0400

    Different Square solution (nlog(n))

--- codesignal.com/arcade/intro/Different Square.js
+++ codesignal.com/arcade/intro/Different Square.js
@@ -40,3 +40,31 @@
 // [output] integer
 
 // The number of different 2 × 2 squares in matrix.
+
+const differentSquares = matrix => {
+  const res = []
+  for(let i = 0; i < matrix.length - 1; i++){
+    for(let j = 0; j < matrix[i].length - 1; j++){
+      const square = []
+      square.push(matrix[i][j])
+      square.push(matrix[i][j + 1])
+      square.push(matrix[i + 1][j])
+      square.push(matrix[i + 1][j + 1])
+      res.push(square)
+    }
+  }
+  if(res.length === 0){
+    return 0
+  }else{
+    res.sort()
+    const dedupedRes = [res[0]]
+    res.forEach(square => {
+      if(!square.every((ele, i) => ele === dedupedRes[dedupedRes.length - 1][i])){
+        dedupedRes.push(square)
+      }
+    })
+    return dedupedRes.length
+  }
+}
+
+// O(nlog(n))
