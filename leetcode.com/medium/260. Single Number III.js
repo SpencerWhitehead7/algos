315f50efabfba97b64cc91659913b36399f8d336
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-23 16:24:27 -0400

    260. Single Number III solution

--- leetcode.com/medium/260. Single Number III.js
+++ leetcode.com/medium/260. Single Number III.js
@@ -7,4 +7,25 @@
 // Note:
 
 // The order of the result is not important. So in the above example, [5, 3] is also correct.
-// Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+// Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity? // Nope, but I imagine it uses XOR
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = nums => {
+  const tracker = {}
+  nums.forEach(num => {
+    if(tracker[num]){
+      tracker[num]++
+    }else{
+      tracker[num] = 1
+    }
+  })
+  const res = []
+  const keys = Object.keys(tracker)
+  for(let i = 0; i < keys.length; i++){
+    if(tracker[keys[i]] === 1) res.push(Number(keys[i]))
+  }
+  return res
+}
