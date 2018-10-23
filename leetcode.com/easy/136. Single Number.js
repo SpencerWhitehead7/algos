a062da0755e9afc589a09aeb6ef03efa89ae7cdd
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-23 15:26:58 -0400

    136. Single Number solution

--- leetcode.com/easy/136. Single Number.js
+++ leetcode.com/easy/136. Single Number.js
@@ -2,7 +2,7 @@
 
 // Note:
 
-// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? // Nope, but I know it uses XOR
 
 // Example 1:
 
@@ -12,3 +12,17 @@
 
 // Input: [4,1,2,1,2]
 // Output: 4
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = nums => {
+  const uniques = new Set(nums)
+  const totalSum = nums.reduce((acc, curr) => acc + curr)
+  let setSum = 0
+  for(const num of uniques){
+    setSum += num
+  }
+  return (setSum * 2) - totalSum
+}
