5799e63c4ffc409a7398e743ea2162722fc1874b
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-22 20:59:12 -0400

    isListPalindrome solution

--- codesignal.com/Interview Practice/Linked Lists/isListPalindrome.js
+++ codesignal.com/Interview Practice/Linked Lists/isListPalindrome.js
@@ -23,3 +23,35 @@
 // [output] boolean
 
 // Return true if l is a palindrome, otherwise return false.
+
+// Definition for singly-linked list:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+//
+const isListPalindrome = l => {
+  let fast = l
+  let slow = l
+  while(fast && fast.next){
+    fast = fast.next.next
+    slow = slow.next
+  }
+  if(fast) slow = slow.next
+  let firstHalf = l
+  let secondHalf = reverseList(slow)
+  while(secondHalf){
+    if(firstHalf.value !== secondHalf.value) return false
+    firstHalf = firstHalf.next
+    secondHalf = secondHalf.next
+  }
+  return true
+}
+
+const reverseList = head => {
+  if(!head || !head.next) return head
+  const rest = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return rest
+}
