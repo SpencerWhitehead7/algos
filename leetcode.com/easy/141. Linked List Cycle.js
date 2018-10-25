d188576c88d9202995c07fdab3369ea3a1861550
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-24 21:22:26 -0400

    141. Linked List Cycle solution

--- leetcode.com/easy/141. Linked List Cycle.js
+++ leetcode.com/easy/141. Linked List Cycle.js
@@ -2,3 +2,26 @@
 
 // Follow up:
 // Can you solve it without using extra space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = head => {
+  if(!head) return false
+  let tortoise = head
+  let hare = head.next
+  while(hare && hare.next && hare !== tortoise){
+    hare = hare.next.next
+    tortoise = tortoise.next
+  }
+  return hare === tortoise
+}
