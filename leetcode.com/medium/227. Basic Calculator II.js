353dd93ccb090b8894e1fee3643a70dc1bd05c2b
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-25 19:40:40 -0400

    227. Basic Calculator II solution

--- leetcode.com/medium/227. Basic Calculator II.js
+++ leetcode.com/medium/227. Basic Calculator II.js
@@ -18,3 +18,44 @@
 
 // You may assume that the given expression is always valid.
 // Do not use the eval built-in library function.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const calculate = s => {
+  s = s.replace(/\s/g, ``)
+  const arr = []
+  let numStart = 0
+  let inNum = false
+  for(let i = 0; i < s.length; i++){
+    if(s[i] === `*` || s[i] === `/` || s[i] === `+` || s[i] === `-`){
+      arr.push(Number(s.slice(numStart, i)))
+      arr.push(s[i])
+      inNum = false
+    }else if(!inNum){
+      numStart = i
+      inNum = true
+    }
+  }
+  arr.push(Number(s.slice(numStart)))
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === `*`){
+      arr.splice(i - 1, 3, arr[i - 1] * arr[i + 1])
+      i -= 3
+    }else if(arr[i] === `/`){
+      arr.splice(i - 1, 3, Math.floor(arr[i - 1] / arr[i + 1]))
+      i -= 3
+    }
+  }
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === `+`){
+      arr[i + 1] = arr[i - 1] + arr[i + 1]
+    }else if(arr[i] === `-`){
+      arr[i + 1] = arr[i - 1] - arr[i + 1]
+    }
+  }
+  return arr[arr.length - 1]
+}
