06726fca672904aa0fe0e60420b58765fa8e58ef
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-10-24 21:08:16 -0400

    142. Linked List Cycle II solution

--- leetcode.com/medium/142. Linked List Cycle II.js
+++ leetcode.com/medium/142. Linked List Cycle II.js
@@ -4,3 +4,44 @@
 
 // Follow up:
 // Can you solve it without using extra space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const detectCycle = head => {
+  if(!head) return null
+  let tortoise = head
+  let hare = head.next
+  while(hare && hare.next && hare !== tortoise){
+    tortoise = tortoise.next
+    hare = hare.next.next
+  }
+  if(!hare || !hare.next) return null
+  let counter = 1
+  hare = hare.next
+  while(hare !== tortoise){
+    hare = hare.next
+    counter++
+  }
+  hare = head
+  tortoise = head
+  let counter2 = 0
+  while(counter2 < counter){
+    hare = hare.next
+    counter2++
+  }
+  while(tortoise !== hare){
+    hare = hare.next
+    tortoise = tortoise.next
+  }
+  return tortoise
+}
