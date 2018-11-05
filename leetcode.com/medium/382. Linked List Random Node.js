cf1404fd7be456d4608cf2f65a9b1dcbfd462b84
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-05 18:44:52 -0500

    382. Linked List Random Node solution

--- leetcode.com/medium/382. Linked List Random Node.js
+++ leetcode.com/medium/382. Linked List Random Node.js
@@ -13,3 +13,45 @@
 
 // // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
 // solution.getRandom();
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+ * @param {ListNode} head
+ */
+const Solution = function(head){
+  this.head = head
+}
+
+/**
+ * Returns a random node's value.
+ * @return {number}
+ */
+Solution.prototype.getRandom = function(){
+  let length = 0
+  let pointer = this.head
+  while(pointer){
+    length++
+    pointer = pointer.next
+  }
+  let randomNode = Math.floor(Math.random() * length)
+  pointer = this.head
+  while(randomNode > 0){
+    randomNode--
+    pointer = pointer.next
+  }
+  return pointer.val
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = Object.create(Solution).createNew(head)
+ * var param_1 = obj.getRandom()
+ */
