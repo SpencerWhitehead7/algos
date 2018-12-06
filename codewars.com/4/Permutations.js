51f697cd75fc13e24862dd804f71cc04fd100eec
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-12-06 01:05:08 -0500

    Permutations solution

--- codewars.com/4/Permutations.js
+++ codewars.com/4/Permutations.js
@@ -6,3 +6,20 @@
 // permutations('ab'); // ['ab', 'ba']
 // permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 // The order of the permutations doesn't matter.
+
+const helper = str => {
+  if(str.length === 1) return [str]
+  const all = []
+  let i = 0
+  while(i < str.length){
+    const letter = str[i]
+    const otherChars = str.slice(0, i) + str.slice(i + 1)
+    helper(otherChars).forEach(subpermut => {
+      all.push(letter + subpermut)
+    })
+    while(str[i] === letter){i++}
+  }
+  return all
+}
+
+const permutations = string => helper(string.split(``).sort().join(``))
