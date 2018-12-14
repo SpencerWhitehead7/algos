128b76cc7a1aa76d2ce9a18207decfbe0d3801a3
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-12-14 02:13:22 -0500

    digitsProduct solution

--- codesignal.com/arcade/intro/digitsProduct.js
+++ codesignal.com/arcade/intro/digitsProduct.js
@@ -16,3 +16,23 @@
 // 0 ≤ product ≤ 600.
 
 // [output] integer
+
+const digitsProduct = product => {
+  if(product === 0) return 10
+  if(product < 10) return product
+  const products = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+  const factors = []
+  while(product !== 1){
+    for(let i = 0; i < products.length; i++){
+      const possibleFactor = products[i]
+      if(possibleFactor === 1) return -1
+      if(Math.floor(product / possibleFactor) === product / possibleFactor){
+        factors.push(possibleFactor)
+        product /= possibleFactor
+        break
+      }
+    }
+  }
+  factors.sort((a, b) => a - b)
+  return Number(factors.join(``))
+}
