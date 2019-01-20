d181269bea1ff0308bd3c022fe883433a4f1997f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2019-01-20 13:28:41 -0700

    935. Knight Dialer solution (matrix)

--- leetcode.com/medium/935. Knight Dialer.js
+++ leetcode.com/medium/935. Knight Dialer.js
@@ -39,25 +39,28 @@
  * @return {number}
  */
 
-const knightDialer = n => {
-  const moves = [
-    [`4`, `6`],
-    [`6`, `8`],
-    [`7`, `9`],
-    [`4`, `8`],
-    [`0`, `3`, `9`],
-    [],
-    [`0`, `1`, `7`],
-    [`2`, `6`],
-    [`1`, `3`],
-    [`2`, `4`],
-  ]
+const MOD = (10 ** 9) + 7
+
+const moves = [
+  [`4`, `6`],
+  [`6`, `8`],
+  [`7`, `9`],
+  [`4`, `8`],
+  [`0`, `3`, `9`],
+  [],
+  [`0`, `1`, `7`],
+  [`2`, `6`],
+  [`1`, `3`],
+  [`2`, `4`],
+]
+
+const knightDialerQueue = n => {
   const queue = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`]
   let count = 0
   while(queue.length > 0){
     const curr = queue.pop()
     if(curr.length === n){
-      count = (count + 1) % (Math.pow(10, 9) + 7)
+      count = (count + 1) % MOD
     }else{
       moves[curr[curr.length - 1]].forEach(possibleHop => {
         queue.push(curr + possibleHop)
@@ -65,4 +68,59 @@ const knightDialer = n => {
     }
   }
   return count
+}
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+const knightDialerMatrix = N => {
+  if(N === 1) return 10
+  let baseMatrix = [
+  // 0,1,2,3,4,5,6,7,8,9
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0], // 0
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 1
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1], // 2
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0], // 3
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1], // 4
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
+    [1, 1, 0, 0, 0, 0, 0, 1, 0, 0], // 6
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0], // 7
+    [0, 1, 0, 1, 0, 0, 0, 0, 0, 0], // 8
+    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0], // 9
+  ]
+  const multMatrix = [
+  // 0,1,2,3,4,5,6,7,8,9
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0], // 0
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 1
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 1], // 2
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0], // 3
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1], // 4
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
+    [1, 1, 0, 0, 0, 0, 0, 1, 0, 0], // 6
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0], // 7
+    [0, 1, 0, 1, 0, 0, 0, 0, 0, 0], // 8
+    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0], // 9
+  ]
+  for(let i = 2; i < N; i++){
+    baseMatrix = multiplyMatrices(baseMatrix, multMatrix)
+  }
+  return baseMatrix.reduce((acc, curr) => acc + (curr.reduce((acc, curr) => acc + curr) % MOD), 0) % MOD
+}
+
+
+const multiplyMatrices = (matrix1, matrix2) => {
+  const res = []
+  for(let row = 0; row < matrix1.length; row++){
+    const newRow = []
+    for(let col = 0; col < matrix1[0].length; col++){
+      const allVals = []
+      matrix1[row].forEach((rowVal, i) => {
+        allVals.push((rowVal * matrix2[i][col]) % MOD)
+      })
+      newRow.push(allVals.reduce((acc, curr) => acc + curr) % MOD)
+    }
+    res.push(newRow)
+  }
+  return res
 }
