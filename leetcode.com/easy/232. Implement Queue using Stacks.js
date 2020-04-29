81bdcc4e3fd945fb6a6c6b0c91cf05699138aa86
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-04-28 22:37:50 -0400

    refac: lint, faster add answer

--- leetcode.com/easy/232. Implement Queue using Stacks.js
+++ leetcode.com/easy/232. Implement Queue using Stacks.js
@@ -22,7 +22,7 @@
 /**
  * Initialize your data structure here.
  */
-const MyQueue = function(){
+const MyQueueSlow = function() {
   this.stack = []
 }
 
@@ -31,7 +31,7 @@ const MyQueue = function(){
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function(x){
+MyQueueSlow.prototype.push = function(x) {
   this.stack.push(x)
 }
 
@@ -39,13 +39,13 @@ MyQueue.prototype.push = function(x){
  * Removes the element from in front of queue and returns that element.
  * @return {number}
  */
-MyQueue.prototype.pop = function(){
+MyQueueSlow.prototype.pop = function() {
   const tempStack = []
-  while(this.stack.length > 0){
+  while (this.stack.length > 0) {
     tempStack.push(this.stack.pop())
   }
   const res = tempStack.pop()
-  while(tempStack.length > 0){
+  while (tempStack.length > 0) {
     this.stack.push(tempStack.pop())
   }
   return res
@@ -55,14 +55,14 @@ MyQueue.prototype.pop = function(){
  * Get the front element.
  * @return {number}
  */
-MyQueue.prototype.peek = function(){
+MyQueueSlow.prototype.peek = function() {
   const tempStack = []
-  while(this.stack.length > 0){
+  while (this.stack.length > 0) {
     tempStack.push(this.stack.pop())
   }
   const res = tempStack.pop()
   this.stack.push(res)
-  while(tempStack.length > 0){
+  while (tempStack.length > 0) {
     this.stack.push(tempStack.pop())
   }
   return res
@@ -72,7 +72,7 @@ MyQueue.prototype.peek = function(){
  * Returns whether the queue is empty.
  * @return {boolean}
  */
-MyQueue.prototype.empty = function(){
+MyQueueSlow.prototype.empty = function() {
   return this.stack.length === 0
 }
 
@@ -84,3 +84,67 @@ MyQueue.prototype.empty = function(){
  * var param_3 = obj.peek()
  * var param_4 = obj.empty()
  */
+
+/**
+ * Initialize your data structure here.
+ */
+const MyQueue = function() {
+  this.enqued = []
+  this.dequed = []
+}
+
+/**
+ * Transfer elements from enque to deque if deque is empty.
+ * @return {void}
+ */
+MyQueue.prototype.transfer = function() {
+  if (!this.dequed.length) {
+    while (this.enqued.length) {
+      this.dequed.push(this.enqued.pop())
+    }
+  }
+}
+
+/**
+* Push element x to the back of queue.
+* @param {number} x
+* @return {void}
+*/
+MyQueue.prototype.push = function(x) {
+  this.enqued.push(x)
+}
+
+/**
+* Removes the element from in front of queue and returns that element.
+* @return {number}
+*/
+MyQueue.prototype.pop = function() {
+  this.transfer()
+  return this.dequed.pop()
+}
+
+/**
+* Get the front element.
+* @return {number}
+*/
+MyQueue.prototype.peek = function() {
+  this.transfer()
+  return this.dequed[this.dequed.length - 1]
+}
+
+/**
+* Returns whether the queue is empty.
+* @return {boolean}
+*/
+MyQueue.prototype.empty = function() {
+  return !this.enqued.length && !this.dequed.length
+}
+
+/**
+* Your MyQueue object will be instantiated and called as such:
+* var obj = new MyQueue()
+* obj.push(x)
+* var param_2 = obj.pop()
+* var param_3 = obj.peek()
+* var param_4 = obj.empty()
+*/
