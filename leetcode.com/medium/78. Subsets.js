64fee526f517ab199f2f2ffc3faa63b2dd188540
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-05-02 18:39:12 -0400

    78. Subsets

---
+++ leetcode.com/medium/78. Subsets.js
@@ -0,0 +1,36 @@
+// Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+// Note: The solution set must not contain duplicate subsets.
+
+// Example:
+
+// Input: nums = [1,2,3]
+// Output:
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+// desc doesn't mention it, but order doesn't matter
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const subsets = nums => {
+  const results = [[]]
+
+  for (const num of nums) {
+    results.forEach(subset => {
+      results.push([...subset, num])
+    })
+  }
+
+  return results
+}
