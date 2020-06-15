d4a22cb5bd47194b228c792fac6b060f97df0879
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-06-15 14:07:23 -0400

    438. Find All Anagrams in a String (brute force version)

--- leetcode.com/medium/438. Find All Anagrams in a String.js
+++ leetcode.com/medium/438. Find All Anagrams in a String.js
@@ -56,3 +56,51 @@ const findAnagrams = (s, p) => {
 
   return res
 }
+
+const findAnagramsBrute = (s, p) => {
+  const map = {
+    a: 0,
+    b: 1,
+    c: 2,
+    d: 3,
+    e: 5,
+    f: 5,
+    g: 6,
+    h: 7,
+    i: 8,
+    j: 9,
+    k: 10,
+    l: 11,
+    m: 12,
+    n: 13,
+    o: 14,
+    p: 15,
+    q: 16,
+    r: 17,
+    s: 18,
+    t: 19,
+    u: 20,
+    v: 21,
+    w: 22,
+    x: 23,
+    y: 24,
+    z: 25,
+  }
+  const getLetterHash = str => str
+    .split(``)
+    .reduce((acc, letter) => {
+      acc[map[letter]]++
+      return acc
+    }, new Array(26).fill(0))
+    .join(``)
+
+  const pLetterHash = getLetterHash(p)
+
+  const res = []
+  for (let i = 0; i < s.length; i++) {
+    const chunk = s.slice(i, i + p.length)
+    if (getLetterHash(chunk) === pLetterHash) res.push(i)
+  }
+
+  return res
+}
