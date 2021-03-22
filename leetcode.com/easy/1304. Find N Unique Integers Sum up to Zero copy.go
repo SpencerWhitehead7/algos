89d5e1858cff655f579ccfafdab55decb8970c9c
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-03-22 00:46:00 -0400

    1304. Find N Unique Integers Sum up to Zero

---
+++ leetcode.com/easy/1304. Find N Unique Integers Sum up to Zero copy.go
@@ -0,0 +1,43 @@
+// 1304. Find N Unique Integers Sum up to Zero
+// Easy
+
+// 518
+
+// 295
+
+// Add to List
+
+// Share
+// Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+// Example 1:
+
+// Input: n = 5
+// Output: [-7,-1,1,3,4]
+// Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+// Example 2:
+
+// Input: n = 3
+// Output: [-1,0,1]
+// Example 3:
+
+// Input: n = 1
+// Output: [0]
+
+// Constraints:
+
+// 1 <= n <= 1000
+
+package algos
+
+func sumZero(n int) []int {
+	var res = []int{}
+	for i := 1; i <= n/2; i++ {
+		res = append(res, i, -i)
+	}
+	if n%2 != 0 {
+		res = append(res, 0)
+	}
+
+	return res
+}
