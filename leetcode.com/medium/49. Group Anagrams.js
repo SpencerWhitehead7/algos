fc7f3cb7ff6ab42fe7f851e4e8451de38a94eea3
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-04-05 23:34:12 -0400

    refac: 49. Group Anagrams simpler implementation

--- leetcode.com/medium/49. Group Anagrams.js
+++ leetcode.com/medium/49. Group Anagrams.js
@@ -21,14 +21,14 @@
 const groupAnagramsSlow = strs => {
   const res = []
   strs.forEach((str, i) => {
-    strs[i] = {str, sorted : str.split(``).sort().join(``)}
+    strs[i] = { str, sorted: str.split(``).sort().join(``) }
   })
   strs.sort((a, b) => a.sort - b.sort)
   const tracker = {}
   strs.forEach(strObj => {
-    if(tracker[strObj.sorted]){
+    if (tracker[strObj.sorted]) {
       tracker[strObj.sorted].push(strObj.str)
-    }else{
+    } else {
       tracker[strObj.sorted] = [strObj.str]
     }
   })
@@ -38,7 +38,7 @@ const groupAnagramsSlow = strs => {
   return res
 }
 
-const groupAnagrams = strs => {
+const groupAnagramsFaster = strs => {
   const res = []
   const baseHash = new Array(26).fill(0)
   strs.forEach((str, i) => {
@@ -46,13 +46,13 @@ const groupAnagrams = strs => {
     str.split(``).forEach(letter => {
       hash[letter.charCodeAt(0) - 97]++
     })
-    strs[i] = {str, hash : hash.join(``)}
+    strs[i] = { str, hash: hash.join(``) }
   })
   const tracker = {}
   strs.forEach(strObj => {
-    if(tracker[strObj.hash]){
+    if (tracker[strObj.hash]) {
       tracker[strObj.hash].push(strObj.str)
-    }else{
+    } else {
       tracker[strObj.hash] = [strObj.str]
     }
   })
@@ -60,4 +60,24 @@ const groupAnagrams = strs => {
     res.push(tracker[key])
   })
   return res
+}
+
+const groupAnagrams = strs => {
+  const baseHash = new Array(26).fill(0)
+  const tracker = strs.reduce((accTracker, str) => {
+    const hash = str.split(``).reduce((accHash, letter) => {
+      accHash[letter.charCodeAt(0) - 97]++
+      return accHash
+    }, [...baseHash])
+
+    if (!accTracker[hash]) {
+      accTracker[hash] = [str]
+    } else {
+      accTracker[hash].push(str)
+    }
+
+    return accTracker
+  }, {})
+
+  return Object.values(tracker)
 }
