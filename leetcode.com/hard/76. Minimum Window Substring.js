9c8b79460ce975ae406f885e5dfd8edd95a2f1ac
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-05-15 13:06:08 -0400

    76. Minimum WIndow Substring

---
+++ leetcode.com/hard/76. Minimum Window Substring.js
@@ -0,0 +1,55 @@
+// Given two strings s and t of lengths m and n respectively, return the minimum window in s which will contain all the characters in t. If there is no such window in s that covers all characters in t, return the empty string "".
+
+// Note that If there is such a window, it is guaranteed that there will always be only one unique minimum window in s.
+
+
+
+// Example 1:
+
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Example 2:
+
+// Input: s = "a", t = "a"
+// Output: "a"
+
+
+// Constraints:
+
+// m == s.length
+// n == t.length
+// 1 <= m, n <= 105
+// s and t consist of English letters.
+
+
+// Follow up: Could you find an algorithm that runs in O(m + n) time?
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+const minWindow = (s, t) => {
+  const windowCharCounts = {}
+  const tCharCounts = Object.entries(t
+    .split(``)
+    .reduce((counts, char) => {
+      counts[char] = (counts[char] ?? 0) + 1
+      return counts
+    }, {}))
+
+  let res = ``
+  let left = 0
+  let right = 0
+  while (right < s.length) {
+    windowCharCounts[s[right]] = (windowCharCounts[s[right]] ?? 0) + 1
+    while (tCharCounts.every(([char, count]) => windowCharCounts[char] >= count)) {
+      if (res === `` || right - left < res.length) res = s.slice(left, right + 1)
+      windowCharCounts[s[left]]--
+      left++
+    }
+    right++
+  }
+
+  return res
+}
