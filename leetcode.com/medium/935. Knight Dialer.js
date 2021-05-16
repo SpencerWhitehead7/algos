9cef5cb2a5188ad52ef32cea8b0c0c283d41077c
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-05-15 23:20:54 -0400

    refac: 935. Knight Dialer simplify DP version names

--- leetcode.com/medium/935. Knight Dialer.js
+++ leetcode.com/medium/935. Knight Dialer.js
@@ -39,32 +39,32 @@
  * @return {number}
  */
 
-const MOD = (10 ** 9) + 7
+const MOD = 10 ** 9 + 7
 
-const moves = [
-  [`4`, `6`],
-  [`6`, `8`],
-  [`7`, `9`],
-  [`4`, `8`],
-  [`0`, `3`, `9`],
+const possibleMoves = [
+  [4, 6],
+  [6, 8],
+  [7, 9],
+  [4, 8],
+  [0, 3, 9],
   [],
-  [`0`, `1`, `7`],
-  [`2`, `6`],
-  [`1`, `3`],
-  [`2`, `4`],
+  [0, 1, 7],
+  [2, 6],
+  [1, 3],
+  [2, 4],
 ]
 
-const knightDialerQueue = n => {
-  const queue = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`]
+const knightDialerStack = n => {
+  const stack = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`]
   let count = 0
-  while(queue.length > 0){
-    const curr = queue.pop()
-    if(curr.length === n){
+  while (stack.length > 0) {
+    const curr = stack.pop()
+    if (curr.length === n) {
       count = (count + 1) % MOD
-    }else{
-      moves[curr[curr.length - 1]].forEach(possibleHop => {
-        queue.push(curr + possibleHop)
-      })
+    } else {
+      for (const nextPos of possibleMoves[curr[curr.length - 1]]) {
+        stack.push(curr + String(nextPos))
+      }
     }
   }
   return count
@@ -75,7 +75,7 @@ const knightDialerQueue = n => {
  * @return {number}
  */
 const knightDialerMatrix = N => {
-  if(N === 1) return 10
+  if (N === 1) return 10
   let baseMatrix = [
   // 0,1,2,3,4,5,6,7,8,9
     [0, 0, 0, 0, 1, 0, 1, 0, 0, 0], // 0
@@ -102,43 +102,46 @@ const knightDialerMatrix = N => {
     [0, 1, 0, 1, 0, 0, 0, 0, 0, 0], // 8
     [0, 0, 1, 0, 1, 0, 0, 0, 0, 0], // 9
   ]
-  for(let i = 2; i < N; i++){
+  for (let i = 2; i < N; i++) {
     baseMatrix = multiplyMatrices(baseMatrix, multMatrix)
   }
-  return baseMatrix.reduce((acc, curr) => acc + (curr.reduce((acc, curr) => acc + curr) % MOD), 0) % MOD
+
+  return baseMatrix.reduce((acc, curr) => acc + curr.reduce((acc, curr) => acc + curr) % MOD, 0) % MOD
 }
 
 const multiplyMatrices = (matrix1, matrix2) => {
   const res = []
-  for(let row = 0; row < matrix1.length; row++){
+  for (let row = 0; row < matrix1.length; row++) {
     const newRow = []
-    for(let col = 0; col < matrix1[0].length; col++){
+    for (let col = 0; col < matrix1[0].length; col++) {
       const allVals = []
       matrix1[row].forEach((rowVal, i) => {
-        allVals.push((rowVal * matrix2[i][col]) % MOD)
+        allVals.push(rowVal * matrix2[i][col] % MOD)
       })
       newRow.push(allVals.reduce((acc, curr) => acc + curr) % MOD)
     }
     res.push(newRow)
   }
+
   return res
 }
 
 /**
- * @param {number} N
+ * @param {number} n
  * @return {number}
  */
-const knightDialer = N => {
-  let dp = new Array(moves.length).fill(1)
-  for(let i = 1; i < N; i++){
-    const newDp = new Array(moves.length).fill(0)
-    for(let j = 0; j < moves.length; j++){
-      moves[j].forEach(move => {
-        newDp[move] += dp[j]
-        newDp[move] %= MOD
-      })
+const knightDialer = n => {
+  let sums = new Array(possibleMoves.length).fill(1)
+  for (let i = 1; i < n; i++) {
+    const nextSums = new Array(possibleMoves.length).fill(0)
+    for (let currPos = 0; currPos < possibleMoves.length; currPos++) {
+      for (const nextPos of possibleMoves[currPos]) {
+        nextSums[nextPos] += sums[currPos]
+        nextSums[nextPos] %= MOD
+      }
     }
-    dp = newDp
+    sums = nextSums
   }
-  return dp.reduce((acc, curr) => acc + curr) % MOD
+
+  return sums.reduce((acc, curr) => (acc + curr) % MOD)
 }
