ebf18646cdfed1b77abb24918cd9d4e4d72c1622
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-08-25 00:33:32 -0400

    res: update eslint pkgs

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+const baseConfig = require("./.eslintrcBase")
+
+module.exports = {
+  ...baseConfig,
+  rules: {
+    ...baseConfig.rules,
+    "no-console": 0,
+    "no-use-before-define": 0
+  }
+}
