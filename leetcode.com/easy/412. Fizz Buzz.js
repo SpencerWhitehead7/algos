8d084fc63a31ea9921d31295bd3b1bea3d6710ee
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2022-10-07 15:34:55 -0400

    412. Fizz Buzz

---
+++ leetcode.com/easy/412. Fizz Buzz.js
@@ -0,0 +1,38 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+// Example 2:
+
+// Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"]
+// Example 3:
+
+// Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+const fizzBuzz = n => {
+  const res = new Array(n)
+
+  for (let v = 1; v <= n; v++) {
+    const i = v - 1
+    if (v % 3 === 0 && v % 5 === 0) {
+      res[i] = `FizzBuzz`
+    } else if (v % 3 === 0) {
+      res[i] = `Fizz`
+    } else if (v % 5 === 0) {
+      res[i] = `Buzz`
+    } else {
+      res[i] = String(v)
+    }
+  }
+
+  return res
+}
