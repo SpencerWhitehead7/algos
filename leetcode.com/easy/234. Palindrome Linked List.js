53732d4a3d360e4badf7eeb9690e3101ee42c00a
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2022-10-25 03:00:42 -0400

    refac: 234. Palindrome Linked List

--- leetcode.com/easy/234. Palindrome Linked List.js
+++ leetcode.com/easy/234. Palindrome Linked List.js
@@ -1,15 +1,20 @@
-// Given a singly linked list, determine if it is a palindrome.
+// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
 
 // Example 1:
 
-// Input: 1->2
-// Output: false
+// Input: head = [1,2,2,1]
+// Output: true
 // Example 2:
 
-// Input: 1->2->2->1
-// Output: true
-// Follow up:
-// Could you do it in O(n) time and O(1) space?
+// Input: head = [1,2]
+// Output: false
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 105].
+// 0 <= Node.val <= 9
+
+// Follow up: Could you do it in O(n) time and O(1) space?
 
 /**
  * Definition for singly-linked list.
@@ -22,6 +27,25 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+const isPalindrome = (head) => {
+  let fast = head
+  let slow = head
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
+  }
+  if (fast) slow = slow.next
+
+  let firstHalf = head
+  let secondHalf = reverseLL(slow)
+  while (secondHalf) {
+    if (firstHalf.val !== secondHalf.val) return false
+    firstHalf = firstHalf.next
+    secondHalf = secondHalf.next
+  }
+
+  return true
+}
 
 const reverseLL = (head) => {
   if (!head || !head.next) return head
@@ -29,25 +53,6 @@ const reverseLL = (head) => {
   head.next.next = head
   head.next = null
   return rest
-}
-
-const isPalindrome = (head) => {
-  if (!head || !head.next) return true
-  let start = head
-  let middle = head
-  let leader = head
-  while (leader && leader.next) {
-    middle = middle.next
-    leader = leader.next.next
-  }
-  if (leader) middle = middle.next
-  middle = reverseLL(middle)
-  while (middle) {
-    if (start.val !== middle.val) return false
-    start = start.next
-    middle = middle.next
-  }
-  return true
 }
 
 const isPalindromeClever = (head) => {
