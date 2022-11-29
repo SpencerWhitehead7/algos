763cf8aabb15becdc29ceceb63a1f90cd763f81e
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2022-11-28 19:42:50 -0500

    refac: 139. Word Break

--- leetcode.com/medium/139. Word Break.js
+++ leetcode.com/medium/139. Word Break.js
@@ -1,24 +1,30 @@
-// Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
 
-// Note:
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
 
-// The same word in the dictionary may be reused multiple times in the segmentation.
-// You may assume the dictionary does not contain duplicate words.
 // Example 1:
 
-// Input: s = "leetcode", wordDict = ["leet", "code"]
+// Input: s = "leetcode", wordDict = ["leet","code"]
 // Output: true
 // Explanation: Return true because "leetcode" can be segmented as "leet code".
 // Example 2:
 
-// Input: s = "applepenapple", wordDict = ["apple", "pen"]
+// Input: s = "applepenapple", wordDict = ["apple","pen"]
 // Output: true
 // Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
-//              Note that you are allowed to reuse a dictionary word.
+// Note that you are allowed to reuse a dictionary word.
 // Example 3:
 
-// Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
 // Output: false
+
+// Constraints:
+
+// 1 <= s.length <= 300
+// 1 <= wordDict.length <= 1000
+// 1 <= wordDict[i].length <= 20
+// s and wordDict[i] consist of only lowercase English letters.
+// All the strings of wordDict are unique.
 
 /**
  * @param {string} s
@@ -26,18 +32,73 @@
  * @return {boolean}
  */
 const wordBreak = (s, wordDict) => {
+  // I don't think the set is algorithmically necessary, but it runs out of time on the "all 'a's" test otherwise
   const checked = new Set()
-  const queue = [``]
-  while (queue.length > 0) {
-    const base = queue.shift()
-    for (const possibleNextWord of wordDict) {
-      const possibleStr = base + possibleNextWord
-      if (possibleStr === s) return true
-      if (s.indexOf(possibleStr) === 0 && !checked.has(possibleStr)) {
-        checked.add(possibleStr)
-        queue.push(possibleStr)
+  const stack = [""]
+  while (stack.length > 0) {
+    const base = stack.pop()
+    for (const word of wordDict) {
+      const candidate = base + word
+      if (candidate === s) return true
+      if (s.startsWith(candidate) && !checked.has(candidate)) {
+        checked.add(candidate)
+        stack.push(candidate)
       }
     }
   }
+
   return false
 }
+
+const wordBreakRec = (s, wordDict) =>
+  // I firmly believe this SHOULD work, but it runs out of time on the "all 'a'"s test
+  s === "" ||
+  wordDict.some(
+    (w) => s.startsWith(w) && wordBreakRec(s.slice(w.length), wordDict)
+  )
+
+const wordBreakRecMemo = (s, wordDict) => {
+  // this DOES work, but the adaptation necessary to add the set is so ugly you should just do it iteratively
+  const checked = new Set()
+
+  const wordBreakRec = (stub, remaining, wordDict) => {
+    if (remaining === "") return true
+
+    for (const word of wordDict) {
+      const candidate = stub + word
+      if (remaining.startsWith(word) && !checked.has(candidate)) {
+        checked.add(candidate)
+        if (wordBreakRec(candidate, remaining.slice(word.length), wordDict)) {
+          return true
+        }
+      }
+    }
+
+    return false
+  }
+
+  return wordBreakRec("", s, wordDict)
+}
+
+const wordBreakDP = (s, wordDict) => {
+  const wordSet = new Set(wordDict)
+  const dps = new Array(s.length + 1).fill(false)
+  dps[0] = true
+
+  for (let i = 1; i < dps.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dps[j] && wordSet.has(s.slice(j, i))) {
+        dps[i] = true
+        break
+      }
+    }
+  }
+
+  return dps[dps.length - 1]
+}
+
+// the evil "all 'a's" test::
+// s =
+// "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+// wordDict =
+// ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
