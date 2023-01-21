596410f2a3b1c4bb6661a105bf8e0d386eecd048
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2023-01-21 01:19:05 -0500

    res: 240. Search a 2D Matrix II (go)

---
+++ leetcode.com/medium/240. Search a 2D Matrix II.go
@@ -0,0 +1,43 @@
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+// Integers in each row are sorted in ascending from left to right.
+// Integers in each column are sorted in ascending from top to bottom.
+
+// Example 1:
+
+// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+// Output: true
+// Example 2:
+
+// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+// Output: false
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= n, m <= 300
+// -109 <= matrix[i][j] <= 109
+// All the integers in each row are sorted in ascending order.
+// All the integers in each column are sorted in ascending order.
+// -109 <= target <= 109
+
+package algos
+
+func searchMatrix(matrix [][]int, target int) bool {
+	ri := len(matrix) - 1
+	ci := 0
+
+	for ri >= 0 && ci <= len(matrix[0])-1 {
+		selected := matrix[ri][ci]
+		if selected > target {
+			ri--
+		} else if selected < target {
+			ci++
+		} else {
+			return true
+		}
+	}
+
+	return false
+}
