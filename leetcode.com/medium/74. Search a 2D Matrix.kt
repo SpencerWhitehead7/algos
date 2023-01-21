f799ec9c6e2039bb1c9130b2274a2d03234140ef
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2023-01-21 00:29:53 -0500

    refac: 74. Search a 2D Matrix

--- leetcode.com/medium/74. Search a 2D Matrix.kt
+++ leetcode.com/medium/74. Search a 2D Matrix.kt
@@ -1,59 +1,43 @@
-// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+// You are given an m x n integer matrix matrix with the following two properties:
 
-// Integers in each row are sorted from left to right.
+// Each row is sorted in non-decreasing order.
 // The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
 // Example 1:
 
-// Input:
-// matrix = [
-//   [1,   3,  5,  7],
-//   [10, 11, 16, 20],
-//   [23, 30, 34, 50]
-// ]
-// target = 3
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 // Output: true
 // Example 2:
 
-// Input:
-// matrix = [
-//   [1,   3,  5,  7],
-//   [10, 11, 16, 20],
-//   [23, 30, 34, 50]
-// ]
-// target = 13
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 // Output: false
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -104 <= matrix[i][j], target <= 104
 
 class Solution {
   fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
-      if(matrix.size == 0 || (matrix.size == 1 && matrix[0].size == 0)) return false
-      var start = 0
-      var end = matrix.size - 1
-      var targetArr: IntArray? = null
-      while(start <= end){
-          val middle = (start + end) / 2
-          if(matrix[middle][matrix[middle].size - 1] < target){
-              start = middle + 1
-          }else if(matrix[middle][0] > target){
-              end = middle - 1
-          }else{
-              targetArr = matrix[middle]
-              break
-          }
-      }
-      if(targetArr == null) return false
-          
-      start = 0
-      end = targetArr.size - 1
-      while(start <= end){
-          val middle = (start + end) / 2
-          if(targetArr[middle] < target ){
-              start = middle + 1
-          }else if(targetArr[middle] > target){
-              end = middle - 1
-          }else{
-              return true
-          }
-      }
-      return false
+    val m = matrix.size
+    val n = matrix[0].size
+
+    var l = 0
+    var r = (m * n) -1
+    while(l <= r){
+      val mid = (l + r) / 2
+      val midV = matrix[mid / n][mid % n]
+
+      if (target == midV) return true
+      if (target < midV) r = mid - 1
+      if (target > midV) l = mid + 1
+    }
+
+    return false
   }
 }
