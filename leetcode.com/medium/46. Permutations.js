74f26ca9c0a4d3ad642b252cea04286d363a4ecf
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2023-02-05 21:56:41 -0500

    refac: 46. Permutations

--- leetcode.com/medium/46. Permutations.js
+++ leetcode.com/medium/46. Permutations.js
@@ -1,31 +1,51 @@
-// Given a collection of distinct integers, return all possible permutations.
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
-// Example:
+// Example 1:
 
-// Input: [1,2,3]
-// Output:
-// [
-//   [1,2,3],
-//   [1,3,2],
-//   [2,1,3],
-//   [2,3,1],
-//   [3,1,2],
-//   [3,2,1]
-// ]
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: [[0,1],[1,0]]
+// Example 3:
+
+// Input: nums = [1]
+// Output: [[1]]
+
+// Constraints:
+
+// 1 <= nums.length <= 6
+// -10 <= nums[i] <= 10
+// All the integers of nums are unique.
 
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
-const permute = (nums, base = []) => {
-  if (!nums.length) return [base]
-  const permutations = []
+const permute = (nums) => {
+  const res = []
+  const temp = []
 
-  for (let i = 0; i < nums.length; i++) {
-    const splicedNums = [...nums]
-    const num = splicedNums.splice(i, 1)
-    permutations.push(...permute(splicedNums, [...base, num]))
+  const recurse = () => {
+    if (temp.length === nums.length) {
+      res.push([...temp])
+      return
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      const num = nums[i]
+      if (num !== null) {
+        temp.push(num)
+        nums[i] = null
+        recurse()
+        temp.pop()
+        nums[i] = num
+      }
+    }
   }
 
-  return permutations
+  recurse()
+
+  return res
 }
