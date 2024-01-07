da3d1b05a36e26e0fa374cfeb27e4be8e0626371
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2024-01-07 00:28:50 -0500

    refac: Coin Change (js)

--- leetcode.com/medium/322. Coin Change.js
+++ leetcode.com/medium/322. Coin Change.js
@@ -1,27 +1,46 @@
-// You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
 
 // Example 1:
 
-// Input: coins = [1, 2, 5], amount = 11
+// Input: coins = [1,2,5], amount = 11
 // Output: 3
 // Explanation: 11 = 5 + 5 + 1
 // Example 2:
 
 // Input: coins = [2], amount = 3
 // Output: -1
-// Note:
-// You may assume that you have an infinite number of each kind of coin.
+// Example 3:
 
+// Input: coins = [1], amount = 0
+// Output: 0
+
+// Constraints:
+
+// 1 <= coins.length <= 12
+// 1 <= coins[i] <= 231 - 1
+// 0 <= amount <= 104
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
 const coinChange = (coins, amount) => {
-  const dpArr = new Array(amount + 1)
-  dpArr[0] = 0
-  for (let i = 1; i <= amount; i++) {
-    dpArr[i] = Infinity
-    coins.forEach((coin) => {
-      if (i - coin >= 0) {
-        dpArr[i] = Math.min(dpArr[i], dpArr[i - coin] + 1)
+  const dp = new Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+
+  for (let dpi = 1; dpi <= amount; dpi++) {
+    for (let ci = 0; ci < coins.length; ci++) {
+      const coin = coins[ci]
+      if (dpi - coin >= 0) {
+        dp[dpi] = Math.min(dp[dpi], dp[dpi - coin] + 1)
       }
-    })
+    }
   }
-  return dpArr[amount] === Infinity ? -1 : dpArr[amount]
+
+  return dp[amount] === Infinity ? -1 : dp[amount]
 }
