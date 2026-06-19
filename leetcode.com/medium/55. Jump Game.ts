cd5d03ed054a11c6267801e4296ab50190cc5000
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2026-06-19 12:43:38 -0400

    maint/refac: update 55. Jump Game.ts

--- leetcode.com/medium/55. Jump Game.ts
+++ leetcode.com/medium/55. Jump Game.ts
@@ -1,33 +1,36 @@
-// Given an array of non-negative integers, you are initially positioned at the first index of the array.
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
-// Each element in the array represents your maximum jump length at that position.
-
-// Determine if you are able to reach the last index.
+// Return true if you can reach the last index, or false otherwise.
 
 // Example 1:
 
-// Input: [2,3,1,1,4]
+// Input: nums = [2,3,1,1,4]
 // Output: true
 // Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 // Example 2:
 
-// Input: [3,2,1,0,4]
+// Input: nums = [3,2,1,0,4]
 // Output: false
-// Explanation: You will always arrive at index 3 no matter what. Its maximum
-//              jump length is 0, which makes it impossible to reach the last index.
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
-const canJump = (nums: number[]) => {
-  if (nums[0] === 0 && nums.length > 1) return false
+// Constraints:
+
+// 1 <= nums.length <= 104
+// 0 <= nums[i] <= 105
+
+const canJump = (nums: number[]): boolean => {
   for (let i = nums.length - 2; i >= 0; i--) {
     if (nums[i] === 0) {
       for (let j = i - 1; j >= 0; j--) {
-        if (nums[j] > i - j) {
+        if (j + nums[j] > i) {
           i = j
           break
         }
-        if (j === 0) return false
       }
+
+      if (nums[i] === 0) return false
     }
   }
+
   return true
 }
